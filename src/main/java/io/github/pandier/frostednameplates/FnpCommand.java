132c805f4942070ec4dde7229430879bfd55c375
pandier <62116820+pandier@users.noreply.github.com>
2025-06-24 12:25:08 +0200

    feat: Add update subcommand that updates all nameplates

--- src/main/java/io/github/pandier/frostednameplates/FnpCommand.java
+++ src/main/java/io/github/pandier/frostednameplates/FnpCommand.java
@@ -15,7 +15,6 @@ import java.util.stream.Stream;
 public final class FnpCommand implements CommandExecutor, TabCompleter {
     private static final String PRIMARY_COLOR = "x5f9fe8".chars().mapToObj(x -> "" + ChatColor.COLOR_CHAR + (char) x).collect(Collectors.joining());
     private static final String SECONDARY_COLOR = "x1c70ed".chars().mapToObj(x -> "" + ChatColor.COLOR_CHAR + (char) x).collect(Collectors.joining());
-    private static final String ICON = "❄";
     private static final String INFO_PREFIX = PRIMARY_COLOR;
 
     private final FrostedNameplates plugin;
@@ -32,13 +31,18 @@ public final class FnpCommand implements CommandExecutor, TabCompleter {
         }
 
         final String subcommand = args[0].toLowerCase(Locale.ROOT);
-        if (subcommand.equals("reload")) {
-            plugin.reloadConfig();
-            sender.sendMessage(INFO_PREFIX + "Successfully reloaded the plugin's configuration");
-            return true;
+        switch (subcommand) {
+            case "reload" -> {
+                plugin.reloadConfig();
+                sender.sendMessage(INFO_PREFIX + "Successfully reloaded the plugin's configuration");
+            }
+            case "update" -> {
+                plugin.updateNameplates();
+                sender.sendMessage(INFO_PREFIX + "Updated all nameplates");
+            }
+            default -> help(sender, label);
         }
 
-        help(sender, label);
         return true;
     }
 
@@ -46,16 +50,19 @@ public final class FnpCommand implements CommandExecutor, TabCompleter {
     public @NotNull List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 1) {
             final String arg = args[0].toLowerCase(Locale.ROOT);
-            return Stream.of("reload").filter(subcommand -> subcommand.startsWith(arg)).toList();
+            return Stream.of("reload", "update")
+                    .filter(subcommand -> subcommand.startsWith(arg))
+                    .toList();
         }
         return List.of();
     }
 
     private void help(@NotNull CommandSender sender, @NotNull String label) {
         final String message = "\n " +
-                PRIMARY_COLOR + ICON + " Frosted " + SECONDARY_COLOR + "Nameplates " + ChatColor.GRAY + "v" + plugin.getDescription().getVersion() +
+                PRIMARY_COLOR + "❄ Frosted " + SECONDARY_COLOR + "Nameplates " + ChatColor.GRAY + "v" + plugin.getDescription().getVersion() +
                 "\n by " + String.join(", ", plugin.getDescription().getAuthors()) + "\n " +
                 PRIMARY_COLOR + "\n /" + label + " reload" + ChatColor.GRAY + " - Reloads the plugin's configuration" +
+                PRIMARY_COLOR + "\n /" + label + " update" + ChatColor.GRAY + " - Updates all nameplates" +
                 "\n ";
         sender.sendMessage(message);
     }
